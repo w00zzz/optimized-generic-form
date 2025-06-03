08c0cd641efd027f95706578aefa05aa2e700a97
AdrianAniel <adrianalpizarperez45@gmail.com>
2025-06-02 21:07:54 -0400

    Configuración de vitest y dependencias en el proyecto

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { configDefaults } from "vitest/config";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./src/testing/setupTests.ts",
+    exclude: [...configDefaults.exclude, "dist/**", "node_modules/**"],
+  },
+});
